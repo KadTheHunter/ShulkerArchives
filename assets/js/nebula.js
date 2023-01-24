b190b4f7d2311ce650908dbf269b73dc4cb2e81f
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-01-23 23:47:59 -0500

    KN Script

---
+++ assets/js/nebula.js
@@ -0,0 +1,7 @@
+function nebulaEntry() {
+    var confirmPassword = "Praise Vesper";
+    var password = document.getElementById("search").value;
+    if (password == confirmPassword) {
+         window.location="html/KædykosNebula.html";
+    }
+}
