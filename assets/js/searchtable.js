57f5915e0f180b2f06fcbf08be82631a6fc2f426
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-01-23 23:10:39 -0500

    Separate searchTable into a separate file

---
+++ assets/js/searchtable.js
@@ -0,0 +1,21 @@
+function searchTable() {
+    var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("kits");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+        if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+          found = true;
+        }
+      }
+      if (found) {
+        tr[i].style.display = "";
+        found = false;
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
