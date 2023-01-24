5e946650f2c0a4a80d88a6fc40924ebc1bd6bf60
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-01-23 23:27:57 -0500

    Fix searchTable removing header

--- assets/js/searchtable.js
+++ assets/js/searchtable.js
@@ -4,7 +4,7 @@ function searchTable() {
     filter = input.value.toUpperCase();
     table = document.getElementById("kits");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
+    for (i = 1; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td");
       for (j = 0; j < td.length; j++) {
         if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
