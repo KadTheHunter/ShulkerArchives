e76f075ab7df7d9e9f476220821bf29b48b3a7b9
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-02-18 18:31:49 -0500

    Debut of the new Archives, via 3gg Utilities Crate

---
+++ assets/js/copy.js
@@ -0,0 +1,32 @@
+const copyButtonLabel = "<img src=\"../../assets/copy.png\">";
+
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerHTML = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+            await copyCode(block, button);
+        });
+    }
+});
+
+async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+
+    // visual feedback that task is completed
+    button.innerText = "Copied";
+
+    setTimeout(() => {
+        button.innerHTML = copyButtonLabel;
+    }, 700);
+}
