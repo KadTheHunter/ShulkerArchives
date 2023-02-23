7547236fb26f5f9b5d4855a730234a9fc3ca0dc9
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-02-23 16:04:13 -0500

    Rewrite UsingNBT  + misc CSS tweaks

---
+++ assets/js/contents.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+			}
+		});
+	});
+
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
+});
