85e2c9b68f5edcb8e24e80fd5ab346741d0df740
Kaddicus <47439649+KadTheHunter@users.noreply.github.com>
2023-02-25 23:13:48 -0500

    Add DataTables, remove misc. JS Junk
    - Added DataTables v1.13.2
      - Added jQuery 3.6.3
    
    - Removed SortTable/SearchTable and all related pieces

--- assets/js/nebula.js
+++
@@ -1,7 +0,0 @@
-function nebulaEntry() {
-    var confirmPassword = atob("UHJhaXNlIFZlc3Blcg==");
-    var password = document.getElementById("search").value;
-    if (password == confirmPassword) {
-        window.location = "html/KædykosNebula.html";
-    }
-}
